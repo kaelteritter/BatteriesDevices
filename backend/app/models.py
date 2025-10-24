5ae0081e42b1ebd5d9863f197e77848ac41e0dd4
Vlad <dev.solobscurus@yandex.ru>
2025-10-24 21:08:15 +0400

    Создание моделей

---
+++ backend/app/models.py
@@ -0,0 +1,33 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Device(Base):
+    """
+    lifespan: в месяцах
+    remaining_capacity: в процентах
+    """
+    __tablename__ = 'devices'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+    nominal_voltage: Mapped[float] = mapped_column(nullable=False)
+    lifespan: Mapped[int] = mapped_column(nullable=False)
+    remaining_capacity: Mapped[float]
+
+
+
+class Battery(Base):
+    __tablename__ = 'batteries'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+    firmare_version: Mapped[str] = mapped_column(nullable=False)
+    is_on: Mapped[bool] = mapped_column(default=False)
+
+    device_id = mapped_column(ForeignKey("devices.id", ondelete="CASCADE"), nullable=True)
+    device = relationship(Device, back_populates="batteries")
