bdddd92ca169802cd71ad35ed069d89b239e14ee
Vlad <dev.solobscurus@yandex.ru>
2025-10-24 21:21:03 +0400

    Настройка подключения к БД

---
+++ backend/app/database.py
@@ -0,0 +1,24 @@
+from os import getenv
+
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+
+
+DB_NAME = getenv('DB_NAME')
+DB_PORT = getenv('DB_PORT')
+DB_HOST = getenv('DB_HOST')
+DB_USER = getenv('DB_USER')
+DB_PASSWORD = getenv('DB_PASSWORD')
+
+DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=False
+)
+
+async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_db():
+    async with async_session() as session:
+        yield session
